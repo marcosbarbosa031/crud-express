df5698fd5fafb1839848f4e8f6022b5c515690d7
Marcos Barbosa <enos_mm@hotmail.com>
2018-04-15 15:10:31 -0300

    heroku db

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    database: process.env.DB_NAME || 'express',
-    user: 'root',
-    password: null,
+    database: process.env.DB_NAME || 'heroku_ab1289e377c98a2',
+    user: 'bcaa1c8748f61e',
+    password: '7c09f915',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'localhost',
+      host: process.env.HOST || 'us-cdbr-iron-east-05.cleardb.net',
       storage: './safe.mysql',
       logging: false
     }
